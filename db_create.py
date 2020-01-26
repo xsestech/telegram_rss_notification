9cea87f6c8a27a464ebec973d8801945d84410f5
xsestech <xsestech@gmail.com>
2020-01-26 16:08:14 +0300

    added database creation

--- db_create.py
+++ db_create.py
@@ -1,11 +1,14 @@
-import sqlalchemy as db
-engine = db.create_engine('sqlite:///users.sqlite')
-connection = engine.connect()
-metadata = db.MetaData()
+import sqlite3
 
-emp = db.Table('users', metadata,
-              db.Column('Id', db.Integer()),
-              db.Column('Url', db.String(255)),
-              )
 
-metadata.create_all(engine) #Creates the table
+connection = sqlite3.connect("users.db", check_same_thread = True)
+cursor = connection.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS "users" (
+	"id"	INTEGER,
+	"url"	TEXT
+)""")
+
+
+connection.commit()
+connection.close()
